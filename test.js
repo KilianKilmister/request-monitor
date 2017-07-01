bc88cecc680ae84851ab1444771e93e44cbe5cba
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-07-01 22:54:08 +0100

    readme

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const TestRunner = require('test-runner')
-const Log = require('./')
+const RequestMonitor = require('./')
 const Lws = require('lws')
 const request = require('req-then')
 
@@ -8,7 +8,7 @@ const runner = new TestRunner()
 runner.test('simple', async function () {
   const port = 8000 + this.index
   const lws = new Lws()
-  const server = lws.create({ port, stack: Log, logFormat: 'combined' })
+  const server = lws.create({ port, stack: RequestMonitor, logFormat: 'combined' })
   const response = await request(`http://localhost:${port}/`)
   server.close()
 })
