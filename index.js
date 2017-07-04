98e7389e7050e95f946db911c549ada75cda7ce2
Lloyd Brookes <lloyd.brookes@gmail.com>
2017-07-04 23:45:02 +0100

    show request data

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = MiddlewareBase => class RequestMonitor extends MiddlewareBase {
   middleware () {
     return async (ctx, next) => {
+      const util = require('util')
+
+      /* first, inspect the request */
       const reqInfo = {
         socketId: ctx.req.socket.id,
         requestId: ctx.req.requestId,
@@ -8,9 +11,19 @@ module.exports = MiddlewareBase => class RequestMonitor extends MiddlewareBase {
         url: ctx.req.url,
         headers: ctx.req.headers
       }
-      if (ctx.request.body) reqInfo.data = ctx.request.body
+      if (ctx.request.rawBody) reqInfo.data = ctx.request.rawBody
+      const incomingBuffer = ctx.req._readableState.buffer
+      if (!reqInfo.data && incomingBuffer.head && incomingBuffer.head.data.length) {
+        reqInfo.data = '__INCOMING__'
+        reqInfo.data[util.inspect.custom] = function (depth, options) {
+          const byteSize = require('byte-size')
+          const size = byteSize(incomingBuffer.head.data.length)
+          return options.stylize(`[ incoming data (${size} so far) ]`, 'special')
+        }
+      }
       this.emit('verbose', 'server.request', reqInfo)
 
+      /* next, inspect the response */
       await next()
       const resInfo = {
         socketId: ctx.req.socket.id,
@@ -22,7 +35,6 @@ module.exports = MiddlewareBase => class RequestMonitor extends MiddlewareBase {
       if (ctx.body) resInfo.data = ctx.body
 
       const stream = require('stream')
-      const util = require('util')
       if (resInfo.data instanceof stream.Readable) {
         resInfo.data[util.inspect.custom] = function (depth, options) {
           return options.stylize(`[ Readable Stream: ${resInfo.data.path} ]`, 'special')
